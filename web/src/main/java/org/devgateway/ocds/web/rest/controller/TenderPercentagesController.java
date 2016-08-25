0911dccf4625613e4889223c95e19ba354e6a5f2
mpostelnicu <mpostelnicu@gmail.com>
2016-08-25 22:15:25 +0300

    OCVN-140

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/TenderPercentagesController.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/TenderPercentagesController.java
@@ -323,5 +323,48 @@ public class TenderPercentagesController extends GenericOCDSController {
         List<DBObject> list = results.getMappedResults();
         return list;
     }
+	
+	
+	@ApiOperation("")
+	@RequestMapping(value = "/api/avgTimeFromPlanToTenderPhase", 
+			method = { RequestMethod.POST, RequestMethod.GET }, produces = "application/json")
+    public List<DBObject> avgTimeFromPlanToTenderPhase(@ModelAttribute 
+    		@Valid final DefaultFilterPagingRequest filter) {
+
+        DBObject project1 = new BasicDBObject();
+        project1.put(Keys.YEAR, new BasicDBObject("$year", "$tender.tenderPeriod.startDate"));
+        project1.put("tender._id", 1);
+        project1.put("planning.budget.amount", 1);
+
+        DBObject group = new BasicDBObject();
+        group.put(Fields.UNDERSCORE_ID, "$year");
+        group.put(Keys.TOTAL_TENDERS, new BasicDBObject("$sum", 1));
+		group.put(Keys.TOTAL_TENDERS_WITH_LINKED_PROCUREMENT_PLAN, new BasicDBObject("$sum", new BasicDBObject("$cond",
+				Arrays.asList(new BasicDBObject("$gt", Arrays.asList("$planning.budget.amount", null)), 1, 0))));
+
+        DBObject project2 = new BasicDBObject();
+        project2.put(Fields.UNDERSCORE_ID, 0);
+        project2.put(Keys.YEAR, Fields.UNDERSCORE_ID_REF);
+        project2.put(Keys.TOTAL_TENDERS, 1);
+        project2.put(Keys.TOTAL_TENDERS_WITH_LINKED_PROCUREMENT_PLAN, 1);
+		project2.put(Keys.PERCENT_TENDERS,
+				new BasicDBObject("$multiply",
+						Arrays.asList(new BasicDBObject("$divide", Arrays.asList(
+								"$" + Keys.TOTAL_TENDERS_WITH_LINKED_PROCUREMENT_PLAN, "$" + Keys.TOTAL_TENDERS)),
+								100)));
+
+        Aggregation agg = newAggregation(
+                match(where("tender.tenderPeriod.startDate").exists(true).
+                		and("planning.budget.amount").exists(true)
+                        .andOperator(getDefaultFilterCriteria(filter))),
+                new CustomProjectionOperation(project1), new CustomGroupingOperation(group),
+                new CustomProjectionOperation(project2),
+                sort(Direction.ASC, Keys.YEAR), skip(filter.getSkip()), limit(filter.getPageSize())
+        );
+
+        AggregationResults<DBObject> results = mongoTemplate.aggregate(agg, "release", DBObject.class);
+        List<DBObject> list = results.getMappedResults();
+        return list;
+    }
 
 }
