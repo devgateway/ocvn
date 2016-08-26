fdf77faf940c8539c9429077e2d7431d3dca7d18
Alexei <savca.alexei@gmail.com>
2016-08-26 19:07:11 +0300

    OCVN-147 Added `Cancelled amount by reason` chart

--- ui/ocvn/tabs/efficiency.jsx
+++ ui/ocvn/tabs/efficiency.jsx
@@ -1,9 +1,10 @@
 import Efficiency from "../../oce/tabs/efficiency";
 import BidPeriod from "../../oce/visualizations/charts/bid-period";
 import Cancelled from "../../oce/visualizations/charts/cancelled";
+import CancelledByReason from "../visualizations/cancelled-by-reason";
 
 class OCVNEfficiency extends Efficiency{}
 
-OCVNEfficiency.visualizations = [BidPeriod, Cancelled];
+OCVNEfficiency.visualizations = [BidPeriod, Cancelled, CancelledByReason];
 
 export default OCVNEfficiency;
