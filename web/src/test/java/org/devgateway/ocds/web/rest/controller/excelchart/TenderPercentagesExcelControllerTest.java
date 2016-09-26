490813a653a582116050ff39b6341ad5f5474733
Ionut Dobre <idobre@developmentgateway.org>
2016-09-26 12:18:12 -0400

    OCVN-307 - add excel chart for *Number of cancelled bids* dashboard.

--- web/src/test/java/org/devgateway/ocds/web/rest/controller/excelchart/TenderPercentagesExcelControllerTest.java
+++ web/src/test/java/org/devgateway/ocds/web/rest/controller/excelchart/TenderPercentagesExcelControllerTest.java
@@ -54,6 +54,33 @@ public class TenderPercentagesExcelControllerTest extends AbstractExcelControlle
     }
 
     @Test
+    public void numberCancelledFundingExcelChart() throws Exception {
+        tenderPercentagesExcelController.numberCancelledFundingExcelChart(
+                new YearFilterPagingRequest(),
+                mockHttpServletResponse);
+
+        final byte[] responseOutput = mockHttpServletResponse.getContentAsByteArray();
+        final Workbook workbook = new XSSFWorkbook(new ByteArrayInputStream(responseOutput));
+        Assert.assertNotNull(workbook);
+
+        final Sheet sheet = workbook.getSheet(ChartType.area.toString());
+        Assert.assertNotNull("check chart type, sheet name should be the same as the type", sheet);
+
+        final XSSFDrawing drawing = (XSSFDrawing) sheet.getDrawingPatriarch();
+        final List<XSSFChart> charts =  drawing.getCharts();
+        Assert.assertEquals("number of charts", 1, charts.size());
+
+        final XSSFChart chart = charts.get(0);
+        Assert.assertEquals("chart title", "Number of cancelled bids", chart.getTitle().getString());
+
+        final List<? extends XSSFChartAxis> axis = chart.getAxis();
+        Assert.assertEquals("number of axis", 2, axis.size());
+
+        final CTChart ctChart = chart.getCTChart();
+        Assert.assertEquals("Check if we have 1 area chart", 1, ctChart.getPlotArea().getAreaChartArray().length);
+    }
+
+    @Test
     public void percentTendersUsingEBidExcelChart() throws Exception {
         tenderPercentagesExcelController.percentTendersUsingEBidExcelChart(
                 new YearFilterPagingRequest(),
