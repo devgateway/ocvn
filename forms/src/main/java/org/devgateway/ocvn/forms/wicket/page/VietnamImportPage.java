06bac66a5beb74c4e0ecaeb8f116dd9314f2e56c
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-09-29 20:41:12 +0300

    OCVN-287

--- forms/src/main/java/org/devgateway/ocvn/forms/wicket/page/VietnamImportPage.java
+++ forms/src/main/java/org/devgateway/ocvn/forms/wicket/page/VietnamImportPage.java
@@ -239,20 +239,24 @@ public class VietnamImportPage extends BasePage {
                 target.add(importContainer);
                 target.add(form);
 
-                try {
+				try {
 
-                    vnExcelImportService.importAllSheets(importForm.getModelObject().getFileTypes(),
-                            importForm.getModelObject().getSourceFiles().getPrototypeDatabaseFile().iterator().next()
-                                    .getContent().getBytes(),
-                            importForm.getModelObject().getSourceFiles().getLocationsFile().iterator().next()
-                                    .getContent().getBytes(),
-                            importForm.getModelObject().getSourceFiles().getPublicInstitutionsSuppliersFile().iterator()
-                                    .next().getContent().getBytes(),
-                            importForm.getModelObject().getDropData(), importForm.getModelObject().getValidateData());
-                } catch (Exception e) {
-                    logger.error(e);
-                    e.printStackTrace();
-                } finally {
+					vnExcelImportService.importAllSheets(importForm.getModelObject().getFileTypes(),
+							importForm.getModelObject().getSourceFiles().getPrototypeDatabaseFile().isEmpty() ? null
+									: importForm.getModelObject().getSourceFiles().getPrototypeDatabaseFile().iterator()
+											.next().getContent().getBytes(),
+							importForm.getModelObject().getSourceFiles().getLocationsFile().isEmpty() ? null
+									: importForm.getModelObject().getSourceFiles().getLocationsFile().iterator().next()
+											.getContent().getBytes(),
+							importForm.getModelObject().getSourceFiles().getPublicInstitutionsSuppliersFile().isEmpty()
+									? null
+									: importForm.getModelObject().getSourceFiles().getPublicInstitutionsSuppliersFile()
+											.iterator().next().getContent().getBytes(),
+							importForm.getModelObject().getDropData(), importForm.getModelObject().getValidateData());
+				} catch (Exception e) {
+					logger.error(e);
+					e.printStackTrace();
+				} finally {
                     target.add(logText);
                     target.add(feedbackPanel);
                     this.setEnabled(false);
