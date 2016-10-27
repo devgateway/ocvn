6cf32fd31181e5e497833d38a647af81990e912b
Alexei <savca.alexei@gmail.com>
2016-10-27 16:16:56 +0300

    OCVN-318 Using `//` instead of `http://`
    (cherry picked from commit 170b5d1)

--- ui/ocvn/visualizations/map/index.jsx
+++ ui/ocvn/visualizations/map/index.jsx
@@ -5,7 +5,7 @@ export let injectOceans = MapClass => class OceansMap extends MapClass{
     getTiles(){
         return (
             <TileLayer
-              url='http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}'
+              url='//server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}'
               attribution='Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri'
             />
         )
