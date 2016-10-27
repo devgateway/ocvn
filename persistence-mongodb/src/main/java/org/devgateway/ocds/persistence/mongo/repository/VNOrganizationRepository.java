931a3ab094dc236634c0ce67c4f0515d1ba5ff4f
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-10-27 14:24:15 +0300

    OCVN-330 added new organization entity, extended from the original with department and group fields

---
+++ persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/repository/VNOrganizationRepository.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package org.devgateway.ocds.persistence.mongo.repository;
+
+import org.devgateway.ocvn.persistence.mongo.dao.VNOrganization;
+
+/**
+ * @author mihai
+ *
+ */
+public interface VNOrganizationRepository extends GenericOrganizationRepository<VNOrganization>  {
+
+}
