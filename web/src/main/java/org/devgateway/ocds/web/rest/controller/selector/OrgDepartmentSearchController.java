8716e30b24abf81965cfef44e223784da545f954
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-10-28 15:37:44 +0300

    OCVN-321 OCVN-322 OCVN-323 OCVN-324

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/selector/OrgDepartmentSearchController.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/selector/OrgDepartmentSearchController.java
@@ -2,13 +2,17 @@ package org.devgateway.ocds.web.rest.controller.selector;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.devgateway.ocds.web.rest.controller.GenericOCDSController;
+import org.devgateway.ocds.web.rest.controller.request.TextSearchRequest;
 import org.devgateway.ocvn.persistence.mongo.dao.OrgDepartment;
 import org.devgateway.ocvn.persistence.mongo.repository.OrgDepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.aggregation.Fields;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +30,16 @@ public class OrgDepartmentSearchController extends GenericOCDSController {
 
     @RequestMapping(value = "/api/ocds/orgDepartment/all", method = { RequestMethod.POST, RequestMethod.GET },
             produces = "application/json")
-    public List<OrgDepartment> orgGroups() {
+    public List<OrgDepartment> orgDepartments() {
 
         return orgDepartmentRepository.findAll(new Sort(Direction.ASC, Fields.UNDERSCORE_ID));
 
+    }
+
+    @RequestMapping(value = "/api/ocds/orgDepartment/search", method = { RequestMethod.POST, RequestMethod.GET },
+            produces = "application/json")
+    public List<OrgDepartment> departmentSearch(@ModelAttribute @Valid final TextSearchRequest request) {
+        return mongoTemplate.find(textSearchQuery(request), OrgDepartment.class);
     }
 
 }
