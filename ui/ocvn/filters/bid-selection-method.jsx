a6dc3fd08a0658348b2f7ee05d2d046ba91eb6fd
Alexei <savca.alexei@gmail.com>
2016-11-10 15:32:18 +0200

    OCVN-329 Fixed wrong labelling of `bid selection method` filters, that used to be labelled `locations`

--- ui/ocvn/filters/bid-selection-method.jsx
+++ ui/ocvn/filters/bid-selection-method.jsx
@@ -2,7 +2,7 @@ import MultipleSelect from "../../oce/filters/inputs/multiple-select";
 
 class BidSelectionMethod extends MultipleSelect{
   getTitle(){
-    return this.t('filters:locations:title');
+    return this.t('filters:bidSelectionMethod:title');
   }
 
   getId(option){
