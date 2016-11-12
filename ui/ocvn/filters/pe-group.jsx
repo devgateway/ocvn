ef6046eabde2a2a4efe44061a398a11219025cd3
Alexei <savca.alexei@gmail.com>
2016-11-12 05:26:00 +0200

    OCVN-327 Added PE group filter component

--- ui/ocvn/filters/pe-group.jsx
+++ ui/ocvn/filters/pe-group.jsx
@@ -2,6 +2,10 @@ import MultipleSelect from "../../oce/filters/inputs/multiple-select";
 
 class PEGroup extends MultipleSelect{
   getTitle(){return this.t('filters:peGroup:title')}
+
+  getId(option){return option.get('id');}
+
+  getLabel(option){return option.get('name');}
 }
 
 PEGroup.ENDPOINT = 'ocds/orgGroup/all';
