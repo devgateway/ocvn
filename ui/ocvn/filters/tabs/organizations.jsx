636f9e7f9b742357b8cc8d57638867ec035f2bcb
Alexei <savca.alexei@gmail.com>
2016-11-12 07:25:36 +0200

    OCVN-326 Added PE department filter to the organizations filter tab

--- ui/ocvn/filters/tabs/organizations.jsx
+++ ui/ocvn/filters/tabs/organizations.jsx
@@ -1,10 +1,12 @@
 import Organizations from "../../../oce/filters/tabs/organizations";
 import PEGroup from "../pe-group";
+import PEDepartment from "../pe-department";
 
 class OCVNOrganizations extends Organizations{}
 
 OCVNOrganizations.FILTERS = Organizations.FILTERS.concat([
-    ['procuringEntityGroupId', PEGroup]
+    ['procuringEntityGroupId', PEGroup],
+    ['procuringEntityDepartmentId', PEDepartment]
 ]);
 
 export default OCVNOrganizations;
