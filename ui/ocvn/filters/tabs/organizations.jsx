d2afe99c093224f27727f622bb128f11ff344a76
Alexei <savca.alexei@gmail.com>
2016-11-12 05:26:28 +0200

    OCVN-327 Added PE group filter to `organizations` filter tab

--- ui/ocvn/filters/tabs/organizations.jsx
+++ ui/ocvn/filters/tabs/organizations.jsx
@@ -4,7 +4,7 @@ import PEGroup from "../pe-group";
 class OCVNOrganizations extends Organizations{}
 
 OCVNOrganizations.FILTERS = Organizations.FILTERS.concat([
-
+    ['procuringEntityGroupId', PEGroup]
 ]);
 
 export default OCVNOrganizations;
