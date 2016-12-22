7c82ca6363ac0b2f641e9cae4c1fbc06e1329012
Mihai Postelnicu <mpostelnicu@gmail.com>
2016-12-22 13:42:28 +0200

    OCE-161 preconditions predicates in constructor

--- persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/flags/processors/release/vietnam/ReleaseFlagI003Processor.java
+++ persistence-mongodb/src/main/java/org/devgateway/ocds/persistence/mongo/flags/processors/release/vietnam/ReleaseFlagI003Processor.java
@@ -4,13 +4,10 @@ import org.devgateway.ocds.persistence.mongo.Award;
 import org.devgateway.ocds.persistence.mongo.FlaggedRelease;
 import org.devgateway.ocds.persistence.mongo.flags.AbstractFlaggedReleaseFlagProcessor;
 import org.devgateway.ocds.persistence.mongo.flags.Flag;
-import org.devgateway.ocds.persistence.mongo.flags.ReleaseFlags;
 import org.devgateway.ocds.persistence.mongo.flags.preconditions.FlaggedReleasePredicates;
-import org.devgateway.ocds.persistence.mongo.flags.preconditions.NamedPredicate;
 import org.devgateway.ocvn.persistence.mongo.dao.VNAward;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 /**
@@ -22,11 +19,17 @@ public class ReleaseFlagI003Processor extends AbstractFlaggedReleaseFlagProcesso
 
     public static final ReleaseFlagI003Processor INSTANCE = new ReleaseFlagI003Processor();
 
+    public ReleaseFlagI003Processor() {
+        preconditionsPredicates = Collections.unmodifiableList(Arrays.asList(
+                FlaggedReleasePredicates.ACTIVE_AWARD,
+                FlaggedReleasePredicates.UNSUCCESSFUL_AWARD,
+                FlaggedReleasePredicates.OPEN_PROCUREMENT_METHOD,
+                FlaggedReleasePredicates.ELECTRONIC_SUBMISSION
+        ));
+    }
+
     @Override
     protected void setFlag(Flag flag, FlaggedRelease flaggable) {
-        if (flaggable.getFlags() == null) {
-            flaggable.setFlags(new ReleaseFlags());
-        }
         flaggable.getFlags().setI003(flag);
     }
 
@@ -37,13 +40,6 @@ public class ReleaseFlagI003Processor extends AbstractFlaggedReleaseFlagProcesso
                 .count();
         rationale.append("Number of eligible unsuccessful awards: ").append(eligibleUnsuccessfulAwards);
         return eligibleUnsuccessfulAwards == 0;
-    }
-
-    @Override
-    protected Collection<NamedPredicate<FlaggedRelease>> getPreconditionsPredicates() {
-        return Collections.unmodifiableList(Arrays.asList(FlaggedReleasePredicates.ACTIVE_AWARD,
-                FlaggedReleasePredicates.UNSUCCESSFUL_AWARD, FlaggedReleasePredicates.OPEN_PROCUREMENT_METHOD,
-                FlaggedReleasePredicates.ELECTRONIC_SUBMISSION));
     }
 
 }
