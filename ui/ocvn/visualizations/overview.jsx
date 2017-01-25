01d83be88717bbdbd0566991d8c49368aa8aa9a6
Alexei <savca.alexei@gmail.com>
2017-01-25 13:41:01 +0200

    OCE-128 Added monthly support for Overview chart's OCVN specific bid plan trace

--- ui/ocvn/visualizations/overview.jsx
+++ ui/ocvn/visualizations/overview.jsx
@@ -1,12 +1,15 @@
 import Overview from "../../oce/visualizations/charts/overview";
-import {pluckImm, response2obj} from "../../oce/tools";
+import {pluckImm, yearlyResponse2obj, monthlyResponse2obj} from "../../oce/tools";
 
 class OCVNOverview extends Overview{
   transform([bidplansResponse, tendersResponse, awardsResponse]){
     const transformed = super.transform([tendersResponse, awardsResponse]);
+    const monthly = bidplansResponse && bidplansResponse[0] && bidplansResponse[0].month;
+    const response2obj = monthly ? monthlyResponse2obj : yearlyResponse2obj;
     const bidplans = response2obj('count', bidplansResponse);
+    const dateKey = monthly ? 'month' : 'year';
     return transformed.map(datum => {
-      datum.bidplan = bidplans[datum.year];
+      datum.bidplan = bidplans[datum[dateKey]];
       return datum;
     })
   }
@@ -14,8 +17,14 @@ class OCVNOverview extends Overview{
   getData(){
     const data = super.getRawData();
     if(!data) return [];
+
+    const monthly = data.hasIn([0, 'month']);
+    const dates = monthly ?
+        data.map(pluckImm('month')).map(month => this.t(`general:months:${month}`)).toArray() :
+        data.map(pluckImm('year')).toArray();
+
     return [{
-      x: data.map(pluckImm('year')).toArray(),
+      x: dates,
       y: data.map(pluckImm('bidplan')).toArray(),
       type: 'scatter',
       name: this.t('charts:overview:traces:bidplan'),
