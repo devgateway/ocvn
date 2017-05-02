e4a438f1c23f1ea611870ee06e60c8ba39c0ca93
Alexei <savca.alexei@gmail.com>
2017-05-02 14:10:24 +0300

    OCE-279 s/budget.projectLocation/projectLocation

--- ui/ocvn/visualizations/map/planned-locations.jsx
+++ ui/ocvn/visualizations/map/planned-locations.jsx
@@ -8,11 +8,11 @@ class PlannedLocations extends Map{
         .groupBy(location => location.getIn(['budget.projectLocation', '_id']))
         .map(locations => locations.reduce((reducedLocation, location) => {
           return {
-            "_id": location.getIn(['budget.projectLocation', '_id']),
-            "name": location.getIn(['budget.projectLocation', 'description']),
+            "_id": location.getIn(['projectLocation', '_id']),
+            "name": location.getIn(['projectLocation', 'description']),
             "amount": reducedLocation.amount + location.get('totalPlannedAmount'),
             "count": reducedLocation.count + location.get('recordsCount'),
-            "coords": location.getIn(['budget.projectLocation', 'geometry', 'coordinates']).toJS()
+            "coords": location.getIn(['projectLocation', 'geometry', 'coordinates']).toJS()
           }
         }, {
           "amount": 0,
