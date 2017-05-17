c2eb2eae58fdb9f412456d08609d84af48194178
Alexei Savca <savca.alexei@gmail.com>
2017-05-17 21:27:35 +0300

    OCE-301 Fixed grouping for tender map

--- ui/ocvn/visualizations/map/planned-locations.jsx
+++ ui/ocvn/visualizations/map/planned-locations.jsx
@@ -5,7 +5,7 @@ class PlannedLocations extends Map{
     let data = super.getData();
     if(!data) return [];
     return data
-        .groupBy(location => location.getIn(['budget.projectLocation', '_id']))
+        .groupBy(location => location.getIn(['projectLocation', '_id']))
         .map(locations => locations.reduce((reducedLocation, location) => {
           return {
             "_id": location.getIn(['projectLocation', '_id']),
